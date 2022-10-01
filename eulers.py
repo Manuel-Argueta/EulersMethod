627bc4208be31763003d8a2b1a4e148df5a36db7
Manuel-Argueta <manuel.argueta0314@gmail.com>
2022-09-30 22:56:40 -0700

    initial commit

---
+++ eulers.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import sympy
+from sympy import *
+
+
+def eulersMethod(differential,step,initX,initY,finalX):
+    currY = initY
+    currX = initX
+    print("( " + str(currX) + "," + str(currY) + " )")
+    xArr = []
+    yArr = []
+    while (currX < finalX):
+        xArr.append(currX)
+        yArr.append(currY)
+        tempDiff = list(differential)
+        valueDict = {"x": str(currX), "y": str(currY), "e": str(math.e), "pi": str(math.pi)}
+        currDifferential = "".join(valueDict.get(ele, ele) for ele in tempDiff)
+        x, y = sympy.symbols('x y')
+        currDifferential = sympify(currDifferential)
+        currY += (currDifferential.evalf())*step
+        currX += step
+        print("( " + str(currX) + "," + str(currY) + " )")
+    plt.plot(xArr, yArr)
+    plt.show()  
+
+
+print("Lets use Eulers Method to approximate solution curves!")
+print("=======================================================")
+userDifferential = str(input("Enter the differential equation: "))
+stepSize = float(input("Enter the step size: "))
+initialX = float(input("Enter the initial X value: "))
+initialY = float(input("Enter the initial Y value: "))
+finalX = float(input("Enter the final X value: "))
+print("=======================================================")
+eulersMethod(userDifferential,stepSize,initialX,initialY,finalX)
+
